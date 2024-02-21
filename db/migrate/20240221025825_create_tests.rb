eabc399639f1754d0f3d905744139474caced7e5
slydevruby <sly.dev@zohomail.com>
2024-02-21 15:43:50 +0700

    fix: t.references :author, null: false, foreign_key: { to_table: :users }
    and rubocop

--- db/migrate/20240221025825_create_tests.rb
+++ db/migrate/20240221025825_create_tests.rb
@@ -2,9 +2,9 @@ class CreateTests < ActiveRecord::Migration[7.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, default:1, null: false
+      t.integer :level, default: 1, null: false
       t.references :category, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
