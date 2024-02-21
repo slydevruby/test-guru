c3e22101721f469e7bacd2a33e110478e276d8b8
slydevruby <sly.dev@zohomail.com>
2024-02-21 09:21:17 +0700

    Revert "Основы rails-моделей"
    This reverts commit e16f1dc8c75914950008142501e0baada13a0ce8.

--- db/migrate/20240220073059_create_answers.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnswers < ActiveRecord::Migration[7.1]
-  def change
-    create_table :answers do |t|
-      t.text :body
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20240220074540_add_correct_to_answers.rb
+++
@@ -1,5 +0,0 @@
-class AddCorrectToAnswers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :answers, :correct, :boolean
-  end
-end
