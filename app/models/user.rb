8ffb01f667d1321cd632a105df98ac2ada7cb3a8
slydevruby <sly.dev@zohomail.com>
2024-02-22 13:29:09 +0700

    feat: добавил инстанс-метод tests, возвращающий список тестов с указанным уровнем

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  def tests(level)
+    tests = Test.where(level: level)
+
+    out = []
+    tests.each do |test|
+      res1 = Result.where(user_id: id)
+      next unless res1.size.positive?
+
+      out << test if res1.where(test_id: test.id).size.positive?
+    end
+    out
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Result.destroy_all
+Test.destroy_all
 Category.destroy_all
+User.destroy_all
 
 %w[Frontend Backend Ruby Javascript].each do |title|
   Category.find_or_create_by(title: title)
 end
 
-User.destroy_all
 %w[Matz John Hank Bruce].each do |user|
   User.create(name: user)
 end
 
 matz_id = User.find_by(name: 'Matz').id
+hank_id = User.find_by(name: 'Hank').id
+bruce_id = User.find_by(name: 'Bruce').id
 
 ruby_id = Category.find_by(title: 'Ruby').id
 
-Test.destroy_all
+level = 1
 ['Basics', 'Types of data', 'OOP', 'Iterators'].each do |theme|
-  Test.create(title: theme, level: 1, category_id: ruby_id, author_id: matz_id)
+  t = Test.create(title: theme, level:  level, category_id: ruby_id, author_id: matz_id)
+  Result.create(test_id: t.id, user_id: hank_id, passed: true)
+  Result.create(test_id: t.id, user_id: bruce_id, passed: false)
+  level += 1
 end
+
+# User.find_by(name: 'Bruce').tests(1)
