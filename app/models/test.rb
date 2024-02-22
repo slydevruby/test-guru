b7f6bbf48979289b4b5105eb32b6b28d5d9badc0
slydevruby <sly.dev@zohomail.com>
2024-02-22 14:03:23 +0700

    feat: Добавил метод класса Test, возвращающий массив имен тестов с указанным именем категории

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.names(cat_name)
+    res = Test.where(category_id: Category.find_by(title: cat_name).id)
+    return unless res
+
+    res.order(title: :desc).pluck(:title)
+  end
 end
