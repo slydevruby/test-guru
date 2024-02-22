b65a6290b7697c034c4a714c9116c2da732b6b38
slydevruby <sly.dev@zohomail.com>
2024-02-22 10:54:35 +0700

    Заполнил значениями модели в db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Category.destroy_all
+
+%w[Frontend Backend Ruby Javascript].each do |title|
+  Category.find_or_create_by(title: title)
+end
+
+User.destroy_all
+%w[Matz John Hank Bruce].each do |user|
+  User.create(name: user)
+end
+
+matz_id = User.find_by(name: 'Matz').id
+
+ruby_id = Category.find_by(title: 'Ruby').id
+
+Test.destroy_all
+['Basics', 'Types of data', 'OOP', 'Iterators'].each do |theme|
+  Test.create(title: theme, level: 1, category_id: ruby_id, author_id: matz_id)
+end
