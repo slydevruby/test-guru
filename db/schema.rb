8ea1ebab4f88e660874cc09e5ffd89c7d55b73d5
slydevruby <sly.dev@zohomail.com>
2024-02-22 11:08:08 +0700

    выполнил миграцию

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_221_031_322) do
-  create_table 'answers', force: :cascade do |t|
-    t.string 'body', null: false
-    t.boolean 'correct', null: false
-    t.integer 'question_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['question_id'], name: 'index_answers_on_question_id'
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_040421) do
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.boolean "correct", null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'title', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'body', null: false
-    t.integer 'test_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['test_id'], name: 'index_questions_on_test_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table 'tests', force: :cascade do |t|
-    t.string 'title', null: false
-    t.integer 'level', default: 1, null: false
-    t.integer 'category_id', null: false
-    t.integer 'author_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_tests_on_author_id'
-    t.index ['category_id'], name: 'index_tests_on_category_id'
+  create_table "results", force: :cascade do |t|
+    t.integer "result"
+    t.boolean "passed", null: false
+    t.integer "test_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_results_on_test_id"
+    t.index ["user_id"], name: "index_results_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", default: 1, null: false
+    t.integer "category_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
-  add_foreign_key 'answers', 'questions'
-  add_foreign_key 'questions', 'tests'
-  add_foreign_key 'tests', 'categories'
-  add_foreign_key 'tests', 'users', column: 'author_id'
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "results", "tests"
+  add_foreign_key "results", "users"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
