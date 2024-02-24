acabc219dc5584c30a6ee41ff4b14ed450436ac7
slydevruby <sly.dev@zohomail.com>
2024-02-24 11:38:06 +0700

    fix: список тестов по уровню

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins("join results on tests.id = results.test_id join users on results.user_id = users.id where tests.level = #{level} and users.name = '#{name}'")
+    Test.joins('join results on tests.id = results.test_id join users on results.user_id = users.id')
+        .where(tests: { level: level }).where(users: { name: name })
   end
 end
