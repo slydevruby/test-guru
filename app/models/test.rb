1601c80871b468e324c6dcad60d18a5d7cf39f09
slydevruby <sly.dev@zohomail.com>
2024-02-25 19:15:19 +0700

    added associations between users, tests, results

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  has_many :results
 
 
   def self.categories_by_title(title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+
   def tests_by_level(level)
     Test.joins('join results on tests.id = results.test_id')
         .where(tests: { level: level })
