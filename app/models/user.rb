205a3df80263cfa1d94641affda09a4143846adb
slydevruby <sly.dev@zohomail.com>
2024-02-26 19:36:36 +0700

    added created tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def tests_by_level(level)
     Test.joins('join results on tests.id = results.test_id')
