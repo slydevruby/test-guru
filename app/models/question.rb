f5be5a54c46bab3b3a8ec48a8a6523651b6aca45
slydevruby <sly.dev@zohomail.com>
2024-02-28 20:58:12 +0700

    validates answers in question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,11 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
+  validate :validates_anwers
+
+  private
+  def validates_anwers
+    errors.add(:answers) unless answers.size in 1..4
+  end
+  
 end
