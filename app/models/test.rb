643174b029845b783adbfb5bd7beb46be99fb129
slydevruby <sly.dev@zohomail.com>
2024-02-28 20:19:26 +0700

    scopes instead of enum; validates title and level

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,14 +6,12 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
 
-  enum :level, { 
-    beginner: 0..1, 
-    intermediate: 2..4, 
-    advanced: 5... 
-  }
-  #scope :beginner, -> { where(level: 0..1) }
-  #scope :intermediate, -> { where(level: 2..4) }
-  #scope :advanced, -> { where(level: 5... ) }
+  validates :title, presence: true
+  validates :level, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  scope :beginner, -> { where(level: 0..1) }
+  scope :intermediate, -> { where(level: 2..4) }
+  scope :advanced, -> { where(level: 5... ) }
 
   scope :category_by_title, -> (title) { joins(:category)
     .where(categories: {title: title})
