ae48fa1b124e67cc056ae4c7b0d41cc76c6b9535
slydevruby <sly.dev@zohomail.com>
2024-02-28 20:21:11 +0700

    validates name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :tests, through: :results, dependent: :destroy
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
+  validates :name, presence: true
+  validates :email, presence: true
+
   def tests_by_level(level)
     tests.where(level: level)
   end
