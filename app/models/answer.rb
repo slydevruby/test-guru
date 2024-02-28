930c31e10ecb04570c64c654ca033ffcf9db4846
slydevruby <sly.dev@zohomail.com>
2024-02-28 20:17:20 +0700

    validates body; a scope instead of an enum

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  enum :correct, { wrong:false, right:true }
+  validates :body, presence: true
 
-#  scope :good, -> { where(correct: true) }
+  scope :right, -> { where(correct: true) }
 end
