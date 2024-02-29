3f6099e1540fcc32aa9b9e8263a0a3c9a2d34a85
slydevruby <sly.dev@zohomail.com>
2024-02-29 18:15:15 +0700

    rewrote uniqueness and categories_by_title

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,9 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: level } 
   validates :level, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
-  validates_uniqueness_of :title, scope: :level
 
   scope :beginner, -> { where(level: 0..1) }
   scope :intermediate, -> { where(level: 2..4) }
@@ -19,5 +18,9 @@ class Test < ApplicationRecord
     .where(categories: {title: title})
     .order(title: :desc)
   }
+
+  def self.categories_by_title(title)
+    category_by_title(title).pluck(:title)
+  end
 
 end
