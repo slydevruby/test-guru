310a248957c5b8625a893f7a84a590cc205d719b
slydevruby <sly.dev@zohomail.com>
2024-02-29 18:10:21 +0700

    added the index for uniqueness title and level

---
+++ db/migrate/20240229104246_add_title_level_to_tests.rb
@@ -0,0 +1,5 @@
+class AddTitleLevelToTests < ActiveRecord::Migration[7.1]
+  def change
+    add_index :tests, [:title, :level], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_122143) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_104246) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_122143) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
