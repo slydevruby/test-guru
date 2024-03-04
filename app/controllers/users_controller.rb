2dca24656d9fcc57323a909afd7e6b633858c79d
slydevruby <sly.dev@zohomail.com>
2024-03-04 08:33:31 +0700

    added User controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+
+  before_action :find_user, except: [:index, :new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Пользователь #{@username} успешно создан"
+      redirect_to users_path
+    else
+      flash[:alert] = @user.errors.full_messages
+      render :new, status: :unprocessable_entity      
+    end
+  end 
+
+  def show
+  end
+
+  def edit
+  end  
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, status: :see_other    
+    else
+      flash[:alert] = @user.errors.full_messages
+      render :edit, status: :unprocessable_entity
+    end
+  end  
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, status: :see_other    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+end
