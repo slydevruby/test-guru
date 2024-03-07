ed9797858fe78a75735d026d972ddc9cae3b755d
slydevruby <sly.dev@zohomail.com>
2024-03-07 14:10:04 +0700

    feat:using helpers, footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    link_to "github", "https://github.com/#{author}/#{repo}", rel: ["nofollow", "noopener"]
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
+
+  def question_header
+    case action_name
+    when 'new'
+      "Create New #{@question.test.title} Question"
+    when 'edit'
+      "Edit #{@question.test.title} Question"
+    end
+    #when 'new' beg = 'Create Test'
+    #when 'edit' beg 'Edit'
+    #end
+    #beg + @question.test.title + ' Question'
+  end
 end
