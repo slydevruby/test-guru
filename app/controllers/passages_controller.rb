14a71d4e2379065e183e93760b8909abc4b38e9a
slydevruby <sly.dev@zohomail.com>
2024-03-13 21:27:33 +0700

    add some methods to 'passage' model & controller

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -1,5 +1,7 @@
 class PassagesController < ApplicationController
 
+  before_action :set_test_passage, only: %i[show update result]
+
   def show
   end
 
@@ -8,6 +10,13 @@ class PassagesController < ApplicationController
   end
 
   def update
-    
+    passage.accept!(params[:answer_ids])
+    render :show
+  end
+
+  private
+
+  def set_test_passage
+    @passage = Passage.find(params[:id])
   end
 end
--- app/models/passage.rb
+++ app/models/passage.rb
@@ -4,4 +4,24 @@ class Passage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question'
+
+  before_validation :before_validation_set_first_question, on: :create
+
+  def accept!(answers_ids)
+    if correct_answer?
+      self.correct_questions += 1
+    end
+    self.current_question = next
+    save
+  end
+
+  private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def correct_answer?(answers_ids)
+    correct_answers.count == correct_answers.where(id: answer_ids).count
+  end
 end
