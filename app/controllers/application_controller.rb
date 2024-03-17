2846b18a36325ec8f9b3dc55a5acd2301e9bd343
slydevruby <slydevruby@zohomail.com>
2024-03-17 11:31:46 +0700

    feat: контроллер сессий, аутентификация

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def authenticate_user!
+    return if current_user
+
+    redirect_to login_path
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
-  # Defines the root path route ("/")
   root 'tests#index'
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+
   resources :users, only: :create
+  resources :sessions, only: :create
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
