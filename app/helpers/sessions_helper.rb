31254e297ad82113cfe7d7e25ea27d3386be9a16
slydevruby <slydevruby@zohomail.com>
2024-03-17 16:44:46 +0700

    Feat:Flash messages helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module SessionsHelper
+  def flash_messages
+    if flash[:alert]
+      content_tag :p, flash[:alert], class: 'flash alert'
+    else
+      content_tag :p, flash[:notice], class: 'flash notice'
+    end
+  end
 end
