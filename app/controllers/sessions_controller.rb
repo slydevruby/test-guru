7495db19119952c3a2e62412c0b8c3cd9db07fa0
slydevruby <slydevruby@zohomail.com>
2024-03-17 20:48:21 +0700

    Fix: redirect to default or original url, clearing session[:return_to]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,28 @@ class SessionsController < ApplicationController
 
   def logout
     session[:user_id] = nil
-    render :new
+    redirect_to login_path
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to session[:return_to]
+      redirect_to_default_or_origin
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password '
       render :new
+    end
+  end
+
+  private
+
+  def redirect_to_default_or_origin
+    if session[:return_to].present?
+      redirect_to session[:return_to]
+      session[:return_to] = nil
+    else
+      redirect_to tests_path
     end
   end
 end
