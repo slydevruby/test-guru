1077596fa77390c5c5ca362fcd85917087cf43c2
slydevruby <slydevruby@zohomail.com>
2024-03-17 17:14:51 +0700

    Fix: changed 'flash_messages' , the 'elsif' branch

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
   def flash_messages
     if flash[:alert]
       content_tag :p, flash[:alert], class: 'flash alert'
-    else
+    elsif flash[:notice]
       content_tag :p, flash[:notice], class: 'flash notice'
     end
   end
