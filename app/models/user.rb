53b62925c371f2deb41f35892298dec572c597bb
slydevruby <slydevruby@zohomail.com>
2024-03-17 17:18:28 +0700

    Feat: email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
 
   has_secure_password
 
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     passages.order(id: :desc).find_by(test_id: test.id)
   end
 
-  def tests_by_level(level)
-    tests.where(level: level)
+  def tests_by_level(lev)
+    tests.where(level: lev)
   end
 end
