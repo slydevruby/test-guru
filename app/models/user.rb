6f370be68451695aec9837d562c83d245ae23302
slydevruby <slydevruby@zohomail.com>
2024-03-20 18:23:01 +0700

    Feat: step 3: rails g devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :passages
   has_many :tests, through: :passages, dependent: :destroy
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
