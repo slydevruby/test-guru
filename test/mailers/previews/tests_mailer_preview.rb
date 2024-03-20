74be54ac2eb9a65f4f589c304a7a897a95028324
slydevruby <slydevruby@zohomail.com>
2024-03-20 21:29:58 +0700

    Feat: preview mailer but it does not work

--- test/mailers/previews/tests_mailer_preview.rb
+++ test/mailers/previews/tests_mailer_preview.rb
@@ -2,4 +2,8 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/tests_mailer
 class TestsMailerPreview < ActionMailer::Preview
+  def completed_test
+    passage = Passage.new(user: User.first, test: Test.first)
+    TestsMailer.completed_test(passage)
+  end
 end
