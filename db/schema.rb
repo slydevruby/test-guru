0f5d9beb8311231e666e0081d73507a71ae90996
slydevruby <slydevruby@zohomail.com>
2024-03-20 18:54:05 +0700

    Feat: changes in the migration and the scheme

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,68 +10,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_316_115_509) do
-  create_table 'answers', force: :cascade do |t|
-    t.string 'body', null: false
-    t.boolean 'correct', null: false
-    t.integer 'question_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['question_id'], name: 'index_answers_on_question_id'
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_112222) do
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.boolean "correct", null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'title', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'passages', force: :cascade do |t|
-    t.integer 'test_id', null: false
-    t.integer 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'correct_questions', default: 0
-    t.integer 'current_question_id'
-    t.index ['current_question_id'], name: 'index_passages_on_current_question_id'
-    t.index ['test_id'], name: 'index_passages_on_test_id'
-    t.index ['user_id'], name: 'index_passages_on_user_id'
+  create_table "passages", force: :cascade do |t|
+    t.integer "test_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "correct_questions", default: 0
+    t.integer "current_question_id"
+    t.index ["current_question_id"], name: "index_passages_on_current_question_id"
+    t.index ["test_id"], name: "index_passages_on_test_id"
+    t.index ["user_id"], name: "index_passages_on_user_id"
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'body', null: false
-    t.integer 'test_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['test_id'], name: 'index_questions_on_test_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table 'tests', force: :cascade do |t|
-    t.string 'title', null: false
-    t.integer 'level', default: 1, null: false
-    t.integer 'category_id', null: false
-    t.integer 'author_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_tests_on_author_id'
-    t.index ['category_id'], name: 'index_tests_on_category_id'
-    t.index %w[title level], name: 'index_tests_on_title_and_level', unique: true
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", default: 1, null: false
+    t.integer "category_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'email'
-    t.string 'password_digest'
-    t.string 'name'
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: ""
+    t.string "name"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'answers', 'questions'
-  add_foreign_key 'passages', 'questions', column: 'current_question_id'
-  add_foreign_key 'passages', 'tests'
-  add_foreign_key 'passages', 'users'
-  add_foreign_key 'questions', 'tests'
-  add_foreign_key 'tests', 'categories'
-  add_foreign_key 'tests', 'users', column: 'author_id'
+  add_foreign_key "answers", "questions"
+  add_foreign_key "passages", "questions", column: "current_question_id"
+  add_foreign_key "passages", "tests"
+  add_foreign_key "passages", "users"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
