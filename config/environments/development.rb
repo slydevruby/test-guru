8c42fb176b7cb5fdbe8c4bad25c229017a31c35a
slydevruby <slydevruby@zohomail.com>
2024-03-20 20:42:01 +0700

    Feat: +gem 'letter_opener', setup mail sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    
 
     config.cache_store = :null_store
   end
@@ -41,6 +41,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.delivery_method = :letter_opener
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
