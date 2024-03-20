a975aca10f2f380b984ed330ad59a7d06a7b2e6c
slydevruby <slydevruby@zohomail.com>
2024-03-20 18:26:32 +0700

    Feat: changes in User.rb - modules for Devise, removed has_secure_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
+
   has_many :passages
   has_many :tests, through: :passages, dependent: :destroy
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
-
-  has_secure_password
 
   def passage(test)
     passages.order(id: :desc).find_by(test_id: test.id)
