6b4036287e528b28da7bbd4ef046a9f57577e3c4
slydevruby <slydevruby@zohomail.com>
2024-03-21 19:49:23 +0700

    Feat: 'admin' layout + style

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
-module Admin
-  class BaseController < ApplicationController
-    before_action :admin_required!
+# rubocop:disable Style/ClassAndModuleChildren
 
-    private
+class Admin::BaseController < ApplicationController
+  layout 'admin'
 
-    def admin_required!
-      redirect_to root_path, alert: 'You are not authorized to view this page' unless current_user.is_a?(Admin)
-    end
+  before_action :admin_required!
+
+  private
+
+  def admin_required!
+    redirect_to root_path, alert: 'You are not authorized to view this page' unless current_user.is_a?(Admin)
   end
 end
+# rubocop:enable Style/ClassAndModuleChildren
--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,62 +1,63 @@
 # frozen_string_literal: true
 
-module Admin
-  class TestsController < Admin::BaseController
-    rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+# rubocop:disable Style/ClassAndModuleChildren
 
-    before_action :set_test, except: %i[index new create]
+class Admin::TestsController < Admin::BaseController
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
-    def index
-      @tests = Test.all
-    end
+  before_action :set_test, except: %i[index new create]
 
-    def new
-      @test = Test.new
-    end
+  def index
+    @tests = Test.all
+  end
 
-    def create
-      @test = Test.new(test_params)
-      if @test.save
-        redirect_to admin_tests_path, notice: "Test #{@test.title} успешно создан"
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
+  def new
+    @test = Test.new
+  end
 
-    def show; end
-
-    def edit; end
-
-    def update
-      if @test.update(test_params)
-        redirect_to admin_tests_path
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      @test.destroy
-      redirect_to admin_tests_path
-    end
-
-    def start
-      current_user.tests.push(@test)
-      redirect_to current_user.passage(@test)
-    end
-
-    private
-
-    def test_params
-      params.require(:test).permit(:title, :level, :category_id, :author_id)
-    end
-
-    def set_test
-      @test = Test.find(params[:id])
-    end
-
-    def rescue_with_test_not_found
-      redirect_to root_path, alert: 'Тест с таким id отсутствует'
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to admin_tests_path, notice: "Test #{@test.title} успешно создан"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.passage(@test)
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    redirect_to root_path, alert: 'Тест с таким id отсутствует'
+  end
 end
+# rubocop:enable Style/ClassAndModuleChildren
