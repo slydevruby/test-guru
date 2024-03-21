0e16691b88f5a173ce05a0d91500a71706101f29
slydevruby <slydevruby@zohomail.com>
2024-03-21 18:17:23 +0700

    Feat: added the migration for STI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_112222) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_111339) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", null: false
@@ -62,7 +62,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_112222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: ""
-    t.string "name"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -76,6 +75,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_112222) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "type", default: "User", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
