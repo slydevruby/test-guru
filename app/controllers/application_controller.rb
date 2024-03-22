472494055005a6b05ffc42b182e51bc04e57d07d
slydevruby <slydevruby@zohomail.com>
2024-03-22 18:58:35 +0700

    Fix: redirect_to admin_test_path for Admin using a helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,13 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
-
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  after_action :store_action
-
-  def store_action
-    return unless request.get?
-
-    if request.path != '/users/sign_in' &&
-       request.path != '/users/sign_up' &&
-       request.path != '/users/password/new' &&
-       request.path != '/users/password/edit' &&
-       request.path != '/users/confirmation' &&
-       request.path != '/users/sign_out' &&
-       !request.xhr? # don't store ajax calls
-      store_location_for(:user, admin_tests_path)
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_tests_path
+    else
+      super
     end
   end
 end
-
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/PerceivedComplexity
