b8f6b7321766a246f16c69ed57dbaa03109d9e21
slydevruby <slydevruby@zohomail.com>
2024-03-26 21:21:32 +0700

    Feat: added pluralized errors, welcome message when admin sign in, 'category' in errors

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,6 +5,8 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     super
+    return unless current_user.is_a?(Admin)
+
     set_flash_message! :notice, :welcome, username: resource.first_name
   end
 end
