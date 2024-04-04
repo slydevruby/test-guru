d4f6497a85206c7b5aefa01457cb17335827f874
slydevruby <slydevruby@zohomail.com>
2024-04-04 18:14:03 +0700

    Feat: добавить модель Gist
    Модель Gist используется для сохранения в БД гистов для конкретного
    вопроса пользователем, проходящим тест

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_21_111339) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_134952) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", null: false
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_111339) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "gists", force: :cascade do |t|
+    t.string "gist_url"
+    t.integer "question_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_gists_on_question_id"
+    t.index ["user_id"], name: "index_gists_on_user_id"
   end
 
   create_table "passages", force: :cascade do |t|
@@ -83,6 +93,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_111339) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "gists", "questions"
+  add_foreign_key "gists", "users"
   add_foreign_key "passages", "questions", column: "current_question_id"
   add_foreign_key "passages", "tests"
   add_foreign_key "passages", "users"
