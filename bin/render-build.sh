4d0883cac4a41d7cd17bdf42c1be60845e6d502c
slydevruby <slydevruby@zohomail.com>
2024-05-06 14:46:52 +0700

    Feat: добавить категории для тестов

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
 bundle exec rails runner "User.find_by(email: 'sly.bart@gmail.com').update_attribute!(:type, 'Admin')"
+bundle exec rails runner "frontend, backend = Category.create!([{ title: 'Frontend' }, { title: 'Backend' }])"
