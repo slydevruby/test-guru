af6405111cae7656a756623e9e43a6abf3155b61
slydevruby <slydevruby@zohomail.com>
2024-05-06 12:16:39 +0700

    Fix: заменить дефолтные настройки почты в development
    заменить дефолтные настройки почты в окружениии development
    для проверки работы почты БЕЗ ГЕМА letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,13 +43,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.credentials.smtp_settings.address,
+    port: Rails.application.credentials.smtp_settings.port,
+    user_name: Rails.application.credentials.smtp_settings.user_name,
+    password: Rails.application.credentials.smtp_settings.password,
+    authentication: Rails.application.credentials.smtp_settings.authentication,
+    enable_starttls_auto: Rails.application.credentials.smtp_settings.enable_starttls_auto
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
