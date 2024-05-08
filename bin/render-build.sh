d7d36a36d18d34846b8a2d6bb7d145def52f21cf
slydevruby <slydevruby@zohomail.com>
2024-05-08 15:29:05 +0700

    Fix: удалить все категории и создать снова

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,4 +8,5 @@ bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 bundle exec rails runner "User.find_by(email: 'sly.bart@gmail.com').update_attribute!(:type, 'Admin')"
-bundle exec rails runner "frontend, backend = Category.create!([{ title: 'Frontend' }, { title: 'Backend' }])"
+bundle exec rails runner "Category.destroy_all"
+bundle exec rails runner "frontend, backend = Category.find_or_create!([{ title: 'Frontend' }, { title: 'Backend' }])"
