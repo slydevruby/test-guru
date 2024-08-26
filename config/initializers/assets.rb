7dca69faecf3445f905c95e517813b4018f70ca6
slydevruby <slydevruby@zohomail.com>
2024-08-26 22:31:28 +0700

    add bootstrap, esbuild, cssbundling, jsbundling, scss-rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap/dist/js')
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
@@ -18,3 +21,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap
 
 # config/initializers/assets.rb
 Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js]
+# Rails.application.config.assets.precompile << "bootstrap.min.js"
