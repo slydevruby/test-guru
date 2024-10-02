18215e6b20db357492ed8c0a6e82c03dcef54c6c
slydevruby <slydevruby@zohomail.com>
2024-10-02 19:32:20 +0700

    Create Badge model

---
+++ db/migrate/20241002123104_create_badges.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateBadges < ActiveRecord::Migration[7.1]
+  def change
+    create_table :badges do |t|
+      t.string :title
+      t.text :url
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_06_102239) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_02_123104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_06_102239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "badges", force: :cascade do |t|
+    t.string "title"
+    t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
