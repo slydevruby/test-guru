fca6a63b7bfb3981ed205025b1963ec3b586e6fe
slydevruby <slydevruby@zohomail.com>
2024-10-02 19:46:29 +0700

    Create associations

--- db/migrate/20241002123323_create_awards.rb
+++ db/migrate/20241002123323_create_awards.rb
@@ -3,7 +3,7 @@ class CreateAwards < ActiveRecord::Migration[7.1]
     create_table :awards do |t|
       t.references :user, null: false, foreign_key: true
       t.references :badge, null: false, foreign_key: true
-      t.integer :count
+      t.integer :count, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_02_123104) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_02_123323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_02_123104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "awards", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "badge_id", null: false
+    t.integer "count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["badge_id"], name: "index_awards_on_badge_id"
+    t.index ["user_id"], name: "index_awards_on_user_id"
   end
 
   create_table "badges", force: :cascade do |t|
@@ -111,6 +121,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_02_123104) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "awards", "badges"
+  add_foreign_key "awards", "users"
   add_foreign_key "gists", "questions"
   add_foreign_key "gists", "users"
   add_foreign_key "passages", "questions", column: "current_question_id"
