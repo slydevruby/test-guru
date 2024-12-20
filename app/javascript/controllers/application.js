f2672dc9498dbfb98333f3dcb8ec13a3902c8736
slydevruby <slydevruby@zohomail.com>
2024-12-20 19:49:22 +0700

    Improve timer

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,17 +1,17 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
+const application = Application.start();
+application.debug = false;
+window.Stimulus = application;
+export { application };
 
-const application = Application.start()
-application.debug = false
-window.Stimulus   = application
-export { application }
-
-
-import ProgressController from "./progress_controller"
+import ProgressController from "./progress_controller";
+import TimeoutController from "./timeout_controller";
 
 Stimulus.handleError = (error, message, detail) => {
-    console.warn(message, detail)
-    ErrorTrackingSystem.captureException(error)
-}
+  console.warn(message, detail);
+  ErrorTrackingSystem.captureException(error);
+};
 
-Stimulus.register("progress", ProgressController)
+Stimulus.register("progress", ProgressController);
+Stimulus.register("timeout", TimeoutController);
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_17_132901) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_12_133440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_17_132901) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "timeout", default: 0, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
